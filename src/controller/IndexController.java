c9061a1d7efcbeb5d5bc19dc7d677c6b04725fba
stwinyk <st_winyk@163.com>
2019-07-23 11:11:48 +0800

    test continue

--- src/controller/IndexController.java
+++ src/controller/IndexController.java
@@ -11,6 +11,7 @@ public class IndexController implements Controller {
 /*
  *  控制类 IndexController实现接口Controller ，提供方法handleRequest处理请求
  *	SpringMVC通过 ModelAndView 对象把模型和视图结合在一起
+ *  哥你是真的帅(修改痕迹)
  */
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ModelAndView mav = new ModelAndView("index.jsp");
